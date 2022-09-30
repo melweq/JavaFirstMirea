ad1906d24a5f854261b8b24dcff6df3d273fd125
melweq <109981803+melweq@users.noreply.github.com>
2022-09-30 19:45:57 +0300

    test commit

---
+++ src/ru/mirea/task1/Number4.java
@@ -0,0 +1,10 @@
+package ru.mirea.task1;
+
+public class Number4 {
+    public static  void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(1 + "/" + i + " ");
+        }
+        System.out.println();
+    }
+}
