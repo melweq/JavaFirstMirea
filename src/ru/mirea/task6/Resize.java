1a83de1bc13e6bf0f315dfc4a27d55a4dd0db2e3
melweq <melweqq@mail.ru>
2022-11-09 14:30:37 +0300

    new

---
+++ src/ru/mirea/task6/Resize.java
@@ -0,0 +1,22 @@
+package ru.mirea.task6;
+
+class Resize implements Command {
+    Shape current;
+    Shape previous;
+
+    Resize(Shape ob) {
+        previous = new Shape();
+        previous.name = ob.name;
+        current = ob;
+    }
+    public void execute() {
+        current.name = "Strange";
+        System.out.println("Old name: " + previous.name);
+        System.out.println("New name: " + current.name);
+    }
+
+    public void unexecute() {
+        current = previous;
+        System.out.println("Now, current name is: " + current.name);
+    }
+}
